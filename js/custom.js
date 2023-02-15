322c0de21266e0e342f8701b7dfab2a7d639730c
chaitanya-shirodkar <chaitanyashirodkar010@gmail.com>
2023-02-15 09:52:26 +0530

    input

--- js/custom.js
+++
@@ -1,5 +0,0 @@
-var canvas = document.getElementById('exampleCanvas'),
-    context = canvas.getContext('2d');
-
-context.font = 'italic 40pt Calibri, sans-serif';
-context.fillText('Hello World!', 50, 50);
